2063293b729035f43ecb2ae894dc665e7dae26b3
pixelready <bizartist@gmail.com>
2022-02-04 11:35:55 -0800

    set up pet model class

--- models.py
+++ models.py
@@ -13,3 +13,21 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+
+class Pet(db.model):
+    """Pet."""
+
+    __tablename__ = "pets"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    species = db.Column(db.String(100), nullable=False)
+    photo_url = db.Column(db.Text, default="", nullable=False)
+    age = db.Column(
+        db.Text,
+        db.CheckConstraint('age in ["baby", "young", "adult", "senior"]'),
+        nullable=False,
+    )
+    notes = db.Column(db.Text)
+    available = db.Column(db.Boolean, nullable=False, default=True)
